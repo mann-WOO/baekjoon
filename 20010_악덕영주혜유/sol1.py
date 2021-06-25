84e7d1d558a3342df00da0d72a77c30407bb0b37
mann_windows <6513032@gmail.com>
2021-06-25 17:00:13 +0900

    add 20010 solutions time out

---
+++ 20010_악덕영주혜유/sol1.py
@@ -0,0 +1,52 @@
+def find_set(x):
+    while x != p[x]:
+        x = p[x]
+    return x
+
+
+N, K = map(int, input().split())
+edges = []
+for _ in range(K):
+    n1, n2, w = map(int, input().split())
+    edges.append((w, n1, n2))
+edges.sort()
+p = [i for i in range(N)]
+cnt = 1
+total = 0
+result_edges = []
+for w, n1, n2 in edges:
+    if find_set(n1) != find_set(n2):
+        result_edges.append((w, n1, n2))
+        cnt += 1
+        total += w
+        if n1 < n2:
+            n1, n2 = n2, n1
+        p[find_set(n1)] = find_set(n2)
+        if cnt == N+1:
+            break
+
+adj_matrix = [[0 for _ in range(N)] for _ in range(N)]
+for (w, n1, n2) in result_edges:
+    adj_matrix[n1][n2] = w
+    adj_matrix[n2][n1] = w
+cost = [[0 for _ in range(N)] for _ in range(N)]
+for i in range(N):
+    q = [i]
+    visited = [0 for _ in range(N)]
+    visited[i] = 1
+    while q:
+        current = q.pop(0)
+        for j in range(N):
+            if adj_matrix[current][j] and not visited[j]:
+                visited[j] = 1
+                cost[i][j] = cost[i][current] + adj_matrix[current][j]
+                q.append(j)
+
+max_cost = 0
+for i in range(N):
+    for j in range(N):
+        if cost[i][j] > max_cost:
+            max_cost = cost[i][j]
+
+print(total)
+print(max_cost)
---
+++ 20010_악덕영주혜유/sol2.py
@@ -0,0 +1,34 @@
+N, K = map(int, input().split())
+adj_matrix = [[0 for _ in range(N)] for _ in range(N)]
+for _ in range(K):
+    n1, n2, w = map(int, input().split())
+    adj_matrix[n1][n2] = w
+    adj_matrix[n2][n1] = w
+costs = [1000000 for _ in range(N)]
+mst = [0 for _ in range(N)]
+
+costs[0] = 0
+mst[0] = 1
+current = 0
+cnt = 1
+sum_costs = 0
+
+while cnt <= N-1:
+    for i in range(N):
+        if adj_matrix[current][i] and adj_matrix[current][i] < costs[i]:
+            costs[i] = adj_matrix[current][i]
+
+    for i in range(N):
+        if not mst[i]:
+            next = i
+            break
+    for i in range(next, N):
+        if costs[i] < costs[next] and not mst[i]:
+            next = i
+
+    mst[next] = 1
+    cnt += 1
+    sum_costs += costs[next]
+    current = next
+
+print(sum_costs)
